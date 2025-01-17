746d12382681c0e484f5ef1291d7c6ffd8dd32d0
unknown <vannoynith12@gmail.com>
2025-01-17 09:53:35 +0700

    second commit

--- config/config.php
+++ config/config.php
@@ -1,4 +1,17 @@
 <?php
+session_start();
+
+    //use this in config file to ensure that this file is restrict from access in 
+
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        //redirect them to desired location
+        header('location: http://localhost/Noshi/index.php');
+        exit;
+    }
+
+
+try{
+    
 
 
     //Database Setup
@@ -16,9 +29,12 @@
 
     $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME.";", USER, PASS);
 
-    if($conn == true){
-        echo "Connected Successfully";
-    } else{
-        echo "Erro Connection";
-    }
+    // if($conn == true){
+    //     echo "Connected Successfully";
+    // } else{
+    //     echo "Erro Connection";
+    // }
 
+} catch(PDOException $e){
+    echo $e-> getMessage();
+}
